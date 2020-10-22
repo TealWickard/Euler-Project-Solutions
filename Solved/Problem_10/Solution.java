44020d6f8f8f8c2fe8b2744988e7a5bfd81d6f05
Teal Wickard <tealwick@gmail.com>
2020-10-22 18:14:37 -0400

    Solution 10's java reverted to template for some reason

--- Solved/Problem_10/Solution.java
+++ Solved/Problem_10/Solution.java
@@ -1,8 +1,19 @@
-package Template;
+package Solved.Problem_10;
 public class Solution {
     public static void main(String[] args){
-        int answer = 0;
-        
+        long answer = 2;
+        int curr = 3;
+        while(curr < 2000000) {
+            if(isPrime(curr)) answer += curr;
+            curr += 2;
+        }
         System.out.printf("The answer is: %s", answer);
+    }
+    /* Taken from Problem 7 */
+    public static boolean isPrime(int num){
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0) return false;
+        }
+        return true;
     }
 }
