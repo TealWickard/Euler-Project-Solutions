e9ec3fd02dcf7fdae2d0df4a1d9a7623d19a2d84
Teal Wickard <tealwick@gmail.com>
2020-10-24 02:56:52 -0400

    Finished Problem 13

---
+++ Problem_13/Solution.java
@@ -0,0 +1,18 @@
+package Problem_13;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws FileNotFoundException {
+        BigInteger answer = new BigInteger("0");
+        Scanner scan = new Scanner(new File("Problem_13/numbers.txt"));
+        while(scan.hasNext()){
+            answer = answer.add(new BigInteger(scan.nextLine()));
+        }
+        scan.close();
+        System.out.printf("The answer is: %s", answer.toString().substring(0, 10));
+    }
+}
