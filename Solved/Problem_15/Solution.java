573cd9d9ea06fc97ac908eb1e748ca2e388a0c61
Teal Wickard <tealwick@gmail.com>
2020-10-24 18:02:39 -0400

    Finished Problem 15

---
+++ Solved/Problem_15/Solution.java
@@ -0,0 +1,16 @@
+package Problem_15;
+
+import java.math.BigInteger;
+
+public class Solution {
+    public static void main(String[] args){
+        BigInteger answer = new BigInteger("1");
+        for(int i = 21; i < 41; i++){
+            answer = answer.multiply(new BigInteger(Integer.toString(i)));
+        }
+        for(int i = 1; i < 21; i++){
+            answer = answer.divide(new BigInteger(Integer.toString(i)));
+        }
+        System.out.printf("The answer is: %s", answer);
+    }
+}
