6e3e1261cf097c44f99b427b8da4eaf2ba488caf
Teal Wickard <tealwick@gmail.com>
2020-10-30 08:48:33 -0400

    Added new isPrime method to usefulFunctions

--- usefulFunctions.java
+++ usefulFunctions.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+
 // Functions I have made that I might need again at some point
 public class usefulFunctions {
     public static boolean isPrime(int num){
         for(int i = 2; i <= Math.sqrt(num); i++){
             if(num % i == 0) return false;
+        }
+        return true;
+    }
+    public static boolean isPrime(int test, ArrayList<Integer> primes){
+        int curr = 0;
+        int prime = primes.get(curr);
+        while(prime <= Math.sqrt(test)){
+            if(test % prime == 0) return false;
+            curr++;
+            prime = primes.get(curr);
         }
         return true;
     }
